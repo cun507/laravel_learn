d6f60e9be380467c0c92a1a627b5dcfaab39223a
cun507 <13831006091@163.com>
2019-04-12 10:57:00 +0800

    忽略

--- ioc_test/superman1.php
+++ ioc_test/superman1.php
@@ -6,27 +6,55 @@
  * Date: 19-4-11
  * Time: 下午6:01
  */
+
+
 function __autoload($classname)
 {
-    $file = $classname . '.php';
+    if (in_array($classname, ['Fight', 'Shot', 'Force'])) {
+        $file = 'Power1.php';
+    } else {
+        $file = $classname . '.php';
+    }
     if (is_file($file)) {
         require_once($file);
     }
 }
+
 class Superman
 {
-    public function __construct()
-    {
+    protected $power;
 
-    }
-
-    function __autoload($classname)
+    public function __construct($modules)
     {
-        $file = $classname . '.php';
-        if (is_file($file)) {
-            require_once($file);
+        $factory = new SuperModuleFactory();
+        foreach ($modules as $moduleName => $moduleOptions) {
+            $this->power[] = $factory->makeModule($moduleName, $moduleOptions);
         }
+    }
+    public function fight(){
+        set_time_limit(0);
+        $num = strval(8662);
+        //var_dump(chunk_split($num,1,' '));exit;
+        //86627817166278171
+        $count = 0;
+        for ($i=1;$i<=$num;$i++){
+            if($i%2 != 0){
+                foreach(explode(' ',chunk_split($i,1,' ')) as $v){
+                    if($v == 3){
+                        $count++;
+                    }
+                }
+            }
+
+        }
+        echo $count;
+        exit;
+        //707829217
+        var_dump($this->power);
     }
 }
 
-$power = new Power1(1,2);
+$super = new Superman([
+    'Fight'=>['100','200']
+]);
+$super->fight();
